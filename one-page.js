3d45329fe2177264d0f6bd052f67c9677828cb6a
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2024-01-06 18:17:54 -0500

    Update one-page.js

--- one-page.js
+++ one-page.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import {login} from "./login.js";
-// import {logout, submit, join, cancel, create_an_arc} from "./dashboard.js";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 import { getDatabase, set, ref, onValue, get, child } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
 var uid;
@@ -28,6 +26,81 @@ var div = document.getElementById("serve");
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+export function login() {
+  signInWithPopup(auth,provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    user = result.user;
+    console.log(credential);
+    console.log(token);
+    console.log(user);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
+window.login = login;
+
+export function logout() {
+  signOut(auth).then(() => {
+  console.log("User is signed out.");
+  window.location.href = "login.html";
+  }).catch((error) => {
+  // An error happened.
+  });
+}
+window.logout = logout;
+export function submit() {
+ var channel_id = Math.floor(Math.random()*99999);
+ let admin = [user_email];   
+ let name = document.getElementById("name").value;
+ set(ref(database, "/channel/" + channel_id + "/members/"),{admin: admin});
+ set(ref(database, "/channel/" + channel_id + "/basic_data"), {name: name});
+ set(ref(database, "users/" + uid + "/channels/" + channel_id), {type: "owner"});
+ var url = new URL("https://jcamille2023.github.io/arc/channel");
+ url.searchParams.append('channel_id', channel_id);
+ console.log(url);
+ window.location.href = url;
+}
+window.submit = submit;
+
+export function join(e) {
+ var url = new URL("https://jcamille2023.github.io/arc/channel");
+ url.searchParams.append('channel_id', e);
+ console.log(url);
+ window.location.href = url;
+}
+window.join = join;
+
+export function cancel() {
+ var div = document.getElementById("add-arcs");
+ div.style.visibility = "hidden";
+ div.innerHTML = "";
+}
+window.cancel = cancel;
+
+export function create_an_arc() {
+ var div = document.getElementById("add-arcs");
+  div.style.visibility = "visible";
+  div.innerHTML = "<div style='padding: 10px;'>" + 
+  "<h1>Create an arc</h1>" + 
+  "<h4>Name</h4>" + 
+  "<input type='text' id='name'></input>" + 
+  '<button onclick="submit()">Submit</button>'+
+  '<button onclick="cancel()">Cancel</button>'+
+  '</div>';
+   
+}
+window.create_an_arc = create_an_arc;
 
 function activate_arc_reference() {
   var arcs_ref = ref(database, "users/" + uid + "/channels/");
