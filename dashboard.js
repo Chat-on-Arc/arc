42ca2b75d3fc5e04ebea850cf6f833329f4436a6
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2024-02-26 00:16:21 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,12 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-import { getDatabase, set, ref, onValue, get, child } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+import { getDatabase, set, ref, onValue, get, child, push } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
+
 var uid;
 var user_email;
+var photoURL;
+let socket;
 
  const firebaseConfig = {
   apiKey: "AIzaSyC5oq9fyPeoo8jVU-N07gYhjt2kFEBGqA8",
@@ -21,7 +24,7 @@ var user_email;
   const auth = getAuth(app);
   const dbRef = ref(getDatabase());
 
-export function logout() {
+function logout() {
   signOut(auth).then(() => {
   console.log("User is signed out.");
   window.location.href = "login.html";
@@ -30,103 +33,122 @@ export function logout() {
   });
 }
 window.logout = logout;
-export function submit() {
- var channel_id = Math.floor(Math.random()*99999);
- let admin = [user_email];   
- let name = document.getElementById("name").value;
- set(ref(database, "/channel/" + channel_id + "/members/"),{admin: admin});
- set(ref(database, "/channel/" + channel_id + "/basic_data"), {name: name});
- set(ref(database, "users/" + uid + "/channels/" + channel_id), {type: "owner"});
- var url = new URL("https://jcamille2023.github.io/arc/channel");
- url.searchParams.append('channel_id', channel_id);
- console.log(url);
- window.location.href = url;
-}
-window.submit = submit;
 
-export function join(e) {
- var url = new URL("https://jcamille2023.github.io/arc/channel");
- url.searchParams.append('channel_id', e);
- console.log(url);
- window.location.href = url;
-}
-window.join = join;
 
-export function cancel() {
- var div = document.getElementById("add-arcs");
- div.style.visibility = "hidden";
- div.innerHTML = "";
-}
-window.cancel = cancel;
-
-export function create_an_arc() {
- var div = document.getElementById("add-arcs");
-  div.style.visibility = "visible";
-  div.innerHTML = "<div style='padding: 10px;'>" + 
-  "<h1>Create an arc</h1>" + 
-  "<h4>Name</h4>" + 
-  "<input type='text' id='name'></input>" + 
-  '<button onclick="submit()">Submit</button>'+
-  '<button onclick="cancel()">Cancel</button>'+
-  '</div>';
-   
-}
-window.create_an_arc = create_an_arc;
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    console.log(user);
-    uid = user.uid;
-    user_email = user.email;
-    document.getElementById("username").innerHTML = user.displayName;
-    document.getElementById("user-greeting").innerHTML = "Hi, " + user.displayName + "!";
-    let basic_info = {
-     displayName: user.displayName,
-     email: user.email,
-    };
-   if(user.email == "milesport@outlook.com") {
-    document.getElememtById("main").innerHTML = "<p>Fatal error: Massive fucking forehead detected.</p><a href='javascript:logout()'>Sign out</a>";
-   }
-   else {
-   set(ref(database, "users/" + uid + "/basic_info"), basic_info);
-    var arcs_ref = ref(database, "users/" + uid + "/channels/");
-    onValue(arcs_ref, (snapshot) => {
-     let data = snapshot.val();
-     console.log(data);
-     let arc_table = document.getElementById("channels-table");
-     arc_table.innerHTML = "";
-     for(let n = 0; n < Object.keys(data).length; n++) {
-      let arc_number = Object.keys(data)[n];
-      get(child(dbRef, '/channel/' + arc_number + "/basic_data")).then((snapshot) => {
-       let arc_data = snapshot.val()
-       let arc = document.createElement("div"); // add to arc_table
-       
-       arc.style.padding = "7px";
-       arc.style.background = "black";
-       
-       let arc_container = document.createElement("div"); // add to arc
-       let arc_name = document.createElement("h3"); // add to arc container
-       let arc_name_node = document.createTextNode(arc_data.name); // add to arc_name
-       
-       arc_name.style.color = "white";
-       let join_arc = document.createElement("button"); // add to arc container
-       join_arc.innerHTML = "Go to arc";
-       join_arc.setAttribute("onclick","join(" + arc_number + ")");
-
-       arc_name.appendChild(arc_name_node);
-       arc_container.appendChild(arc_name);
-       arc_container.appendChild(join_arc);
-       arc.appendChild(arc_container);
-       arc_table.appendChild(arc);
-      });
-     }
+ function create_an_arc() {
+    let user = auth.currentUser;
+    var div = document.getElementById("add-arcs");
+     div.style.visibility = "visible";
+    let name_input = document.createElement('input');
+    name_input.setAttribute("id","name");
+    let submit_button = document.createElement("button");
+    submit_button.innerHTML = "Submit";
+    let cancel_button = document.createElement("button");
+    cancel_button.innerHTML = "Cancel";
+    submit_button.addEventListener('click', async () => {
+        let name = document.getElementById("name").value;
+        let token = await user.getIdToken();
+        console.log(token);
+        console.log("Submit attempt");
+        socket.timeout(5000).emit('new circle',name, token, (err, res) => {
+            if (err) {
+                console.log("The server failed to respond.");
+            }
+            else {
+                if (res.status != 'failed') {
+                    let circle_id = res.circle_id;
+                    
+                }
+                else {
+                   console.log("failed");
+                }
+            }
+         });
     });
+    cancel_button.addEventListener("onclick", cancel());
+
+     div.innerHTML =  "<h1>Create an arc</h1>" + 
+     "<h4>Name</h4>";
+    div.appendChild(name_input);
+    div.appendChild(submit_button);
+    div.appendChild(cancel_button);
+
+      
    }
-    // ...
-  } else {
-    window.location.href = "index.html";
-    // ...
+   window.create_an_arc = create_an_arc;
+
+   function cancel() {
+    var div = document.getElementById("add-arcs");
+    div.style.visibility = "hidden";
+    div.innerHTML = "";
+   }
+   window.cancel = cancel;
+   
+   function new_direct() {
+    var div = document.getElementById("add-arcs");
+    div.style.visibility = "visible";
+    div.innerHTML = "<div style='padding: 10px;'>" + 
+    "<h1>New conversation</h1>" + 
+    "<h4>User email</h4>" + 
+    "<input type='text' id='email-input'></input>" + 
+    '<button onclick="create_direct()">Submit</button>'+
+    '<button onclick="cancel()">Cancel</button>'+
+    '</div>';
   }
+  window.new_direct = new_direct;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      console.log(user);
+      
+      uid = user.uid;
+      photoURL = user.photoURL;
+      if(photoURL != null) {
+        setPicture(photoURL);
+      }
+      user_email = user.email;
+      document.getElementById("username").innerHTML = user.displayName;
+      document.getElementById("user-greeting").innerHTML = "Hi, " + user.displayName + "!";
+      let basic_info = {
+       displayName: user.displayName,
+       email: user.email,
+      };
+     if(user.email == "milesport@outlook.com") {
+      document.getElememtById("main").innerHTML = "<p>Fatal error: Massive fucking forehead detected.</p><a href='javascript:logout()'>Sign out</a>";
+     }
+     user.getIdToken().then((token) => {
+        const auth_data = {token: token}; 
+        socket = io("ws://localhost:3000", {
+            reconnectionDelayMax: 1000,
+            auth: auth_data,
+            query: {
+            "my-key": "value",
+            }
+     });
+     socket.timeout(5000).emit('circle_list',token, (err, res) => {
+        if (err) {
+            console.log("The server failed to respond.");
+        }
+        else {
+            if (res.status != 'none') {
+                console.log(res.list);
+
+            }
+            else {
+                document.getElementById("channels-section").innerHTML = "You are not in any Circles.";
+            }
+        }
+     });
+     
 });
+
+    }
+    else {
+        console.log("User is signed out");
+    }
+});
+
+
+
+
